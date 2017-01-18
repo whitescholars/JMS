3846f5ca96407ce9dc94231e868d97aabd1f56a8
penglei <742270175@qq.com>
2017-01-18 17:06:36 +0800

    fix

---
+++ ActiveMq/src/main/java/com/penglei/service/ConsumerService.java
@@ -0,0 +1,29 @@
+package com.penglei.service;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+/**
+ * Created by pl on 2017/1/18.
+ */
+@Service
+public class ConsumerService {
+    @Resource(name="jmsTemplate")
+    private JmsTemplate jmsTemplate;
+
+    public TextMessage receive(Destination destination){
+        TextMessage textMessage = (TextMessage) jmsTemplate.receive(destination);
+        try{
+            System.out.println("从队列" + destination.toString() + "收到了消息：\t"
+                    + textMessage.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return textMessage;
+    }
+}
